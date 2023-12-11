303fa29e195512618080c3b2098417b5dbd49231
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-12-12 00:40:24 +0100

    Create a table for storing messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+greetings = [
+  "Hello!",
+  "Welcome!",
+  "Greetings!",
+  "Good day!",
+  "Hi there!"
+]
+
+greetings.each do |greeting|
+  Message.create(content: greeting)
+end
